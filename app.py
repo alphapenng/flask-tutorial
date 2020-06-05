a396bba94805d5a41d5b70229524acf922342cea
alphapenng <barca8best@gmail.com>
2020-06-05 09:49:37 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 '''
-@Author: barca8best
+@Author: alphapenng
 @Date: 2020-06-04 16:11:13
-@LastEditors: barca8best
-@LastEditTime: 2020-06-04 17:43:17
+@LastEditors: alphapenng
+@LastEditTime: 2020-06-05 09:41:48
 @Description: 
 '''
-from flask import Flask
-from flask import url_for
+from flask import Flask, render_template
+
+name = 'alphapenng'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'}
+]
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' %name
-
-@app.route('/test')
-def test_url_for():
-    return 'Test page'
-
+def index():
+    return render_template('index.html', name=name, movies=movies)
